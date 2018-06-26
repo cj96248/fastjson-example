0609361a69d97060e5410fc4f8ad51b279c9f56d
cj96248 <253037585@qq.com>
2018-06-26 22:47:24 +0800

    Add to json

--- src/test/java/com/training/deserialize/ArrayToJSON.java
+++ src/test/java/com/training/deserialize/ArrayToJSON.java
@@ -1,5 +1,25 @@
 package com.training.deserialize;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.alibaba.fastjson.JSON;
+
 public class ArrayToJSON {
 
+	@Test
+	public void test1(){
+		List<String> list = new ArrayList<String>();
+		list.add("ATM");
+		
+		String jsonString = JSON.toJSONString(list);
+		System.out.println(jsonString);//["ATM"]
+		
+		list.add("RMB");
+		
+		String js = JSON.toJSONString(list);
+		System.out.println(js);//["ATM","RMB"]
+	}
 }
--- src/test/java/com/training/deserialize/ObjectToJSON.java
+++ src/test/java/com/training/deserialize/ObjectToJSON.java
@@ -1,5 +1,26 @@
 package com.training.deserialize;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.alibaba.fastjson.JSON;
+
 public class ObjectToJSON {
 
+	@Test
+	public void test1(){
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("name", "Zhangsan");
+		map.put("nickname","xiaozhang");
+		
+		String jsonString = JSON.toJSONString(map);
+		System.out.println(jsonString);//{"name":"Zhangsan","nickname":"xiaozhang"}
+	}
+	
+	@Test
+	public void test2(){
+		Map<String, String> map = new HashMap<>();
+	}
 }
